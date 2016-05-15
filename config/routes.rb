eeb0089ec2c7526502746afec98c95421fff70ee
Sam Assadi <samsassadi@gmail.com>
2016-05-15 10:36:33 -0700

    active user/blogs

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   devise_for :users, class_name: 'FormUser', :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
-  # resources 'users', except: [:index] do
-  #   resources :blogs, except: [:index]
-  # end
+  resources 'users', except: [:index] do
+    resources :blogs, except: [:index]
+  end
 end
